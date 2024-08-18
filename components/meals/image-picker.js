95f1af513344ce56f5ffe62a0d6d2ac5d0490ad4
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-18 22:38:21 +0900

    Storing Server Actions in Separate Files

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,7 +1,7 @@
 "use client";
+import Image from "next/image";
 import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
-import Image from "next/image";
 
 function ImagePicker({ label, name }) {
 	const [pickedImage, setPickedImage] = useState();
