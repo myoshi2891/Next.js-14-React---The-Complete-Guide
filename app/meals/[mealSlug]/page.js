4869a52819b23fbb79b2501f94da4a33e97b5cb8
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-20 22:17:03 +0900

    Adding Dynamic Metadata

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -3,6 +3,18 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import classes from "./page.module.css";
 
+export async function generateMetadata({ params }) {
+	const meal = getMeal(params.mealSlug);
+
+	if (!meal) {
+		notFound();
+	}
+	return {
+		title: meal.title,
+		description: meal.summary,
+	};
+}
+
 function MealDetailsPage({ params }) {
 	const meal = getMeal(params.mealSlug);
 
