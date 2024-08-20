59175efd0485371df9c06d46f2e253e153c9f167
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-20 22:40:00 +0900

    Exercise Solution - Part 1

--- app/news/[id]/page.js
+++ app/news/[id]/page.js
@@ -1,5 +1,11 @@
-function NewsDetailPage() {
-	return <div>detail</div>;
+function NewsDetailPage({ params }) {
+	const newsId = params.id;
+	return (
+		<>
+			<h1>News Details Page</h1>
+			<p>News ID: {newsId}</p>
+		</>
+	);
 }
 
 export default NewsDetailPage;
--- app/news/page.js
+++ app/news/page.js
@@ -1,12 +1,21 @@
 import Link from "next/link";
 
 function NewsPage() {
-	return (
-		<ul>
-			<Link href={`/news/${id}`}>test</Link>
-			<Link href={`/news/${id}`}>test</Link>
-			<Link href={`/news/${id}`}>test</Link>
-		</ul>
+    return (
+		<>
+			<h1>News Page</h1>
+			<ul>
+				<li>
+					<Link href="/news/first-news">First News Item</Link>
+				</li>
+				<li>
+					<Link href="/news/second-news">Second News Item</Link>
+				</li>
+				<li>
+					<Link href="/news/third-news">Third News Item</Link>
+				</li>
+			</ul>
+		</>
 	);
 }
 
