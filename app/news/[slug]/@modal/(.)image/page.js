d7bada088815a9aa8a4af8ae6fe27cc7a626fcfe
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-08-23 17:16:33 +0900

    Navigating Programmatically

--- app/news/[slug]/@modal/(.)image/page.js
+++ app/news/[slug]/@modal/(.)image/page.js
@@ -1,7 +1,10 @@
+"use client";
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 
 function InterceptedImagePage({ params }) {
+	const router = useRouter();
+
 	const newsItemSlug = params.slug;
 	const newsItem = DUMMY_NEWS.find(
 		(newsItem) => newsItem.slug === newsItemSlug
@@ -11,7 +14,7 @@ function InterceptedImagePage({ params }) {
 
 	return (
 		<>
-			<div className="modal-backdrop" />
+			<div className="modal-backdrop" onClick={router.back} />
 			<dialog className="modal" open>
 				<div className="fullscreen-image">
 					<img
