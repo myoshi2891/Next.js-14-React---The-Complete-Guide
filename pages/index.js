11cc43b1df077b6b0ec0237c46849af49c8b8072
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-01 11:37:31 +0900

    Adding a Filter Form for Filtering Events

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import EventList from "../components/events/event-list";
+import EventsSearch from "../components/events/events-search";
 import { getFeaturedEvents } from "../dummy-data";
 
 function HomePage() {
 	const featuredEvents = getFeaturedEvents();
 
 	return (
-		<div>
+		<>
+			<EventsSearch />
 			<EventList items={featuredEvents} />
-		</div>
+		</>
 	);
 }
 
