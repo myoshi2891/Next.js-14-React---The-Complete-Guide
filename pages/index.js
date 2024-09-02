9f4ed1fdb9f249a1d7a1a884426905d9308b1efb
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-02 23:26:07 +0900

    Preparations

--- pages/index.js
+++ pages/index.js
@@ -1,47 +1,14 @@
-import fs from "fs/promises";
-import Link from "next/link";
-import path from "path";
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
 
-function HomePage(props) {
-	const { products } = props;
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
 
-	return (
-		<ul>
-			{products.map((product) => (
-				<li key={product.id}>
-					<Link href={`/products/${product.id}`}>
-						{product.title}
-					</Link>
-				</li>
-			))}
-		</ul>
-	);
-}
-
-export async function getStaticProps(context) {
-	console.log("ReGenerating...");
-	const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-	const jsonData = await fs.readFile(filePath);
-	const data = JSON.parse(jsonData);
-
-	if (!data) {
-		return {
-			redirect: {
-				destination: "/",
-			},
-		};
-	}
-
-	// if (data.products.length === 0) {
-	// 	return { notFound: true };
-	// }
-
-	return {
-		props: {
-			products: data.products,
-		},
-		revalidate: 10,
-	};
+  return (
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
+  );
 }
 
 export default HomePage;
