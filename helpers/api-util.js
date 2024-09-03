2c5078ba30f9af2dbf334bf9c2d965527c4674b1
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-03 22:27:11 +0900

    Adding Static Site Generation (SSG) On The Home Page

---
+++ helpers/api-util.js
@@ -0,0 +1,19 @@
+export async function getAllEvents() {
+	const response = await fetch(process.env.NEXT_PUBLIC_URL);
+	const data = await response.json();
+	const events = [];
+
+	for (const key in data) {
+		events.push({
+			id: key,
+			...data[key],
+		});
+	}
+
+	return events;
+}
+
+export async function getFeaturedEvents() {
+	const allEvents = await getAllEvents();
+	return allEvents.filter((event) => event.isFeatured);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,20 @@
-import { getFeaturedEvents } from '../dummy-data';
-import EventList from '../components/events/event-list';
+import { getFeaturedEvents } from "../helpers/api-util";
+import EventList from "../components/events/event-list";
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
-  return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
-  );
+function HomePage(props) {
+	return (
+		<div>
+			<EventList items={props.events} />
+		</div>
+	);
 }
 
+export async function getStaticProps() {
+	const featuredEvents = await getFeaturedEvents();
+	return {
+		props: {
+			events: featuredEvents,
+		},
+	};
+}
 export default HomePage;
