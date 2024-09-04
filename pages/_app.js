64392cd3015debce045512c7a9496d95198f07ed
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-04 22:54:02 +0900

    Writing Our First API Route

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,7 @@
-import Head from "next/head";
-import Layout from "../components/layout/layout";
-import "../styles/globals.css";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<Layout>
-			<Head>
-				<title>Next Events</title>
-				<meta name="description" content="NextJs Events" />
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1.0"
-				/>
-			</Head>
-			<Component {...pageProps} />
-		</Layout>
-	);
+  return <Component {...pageProps} />
 }
 
-export default MyApp;
+export default MyApp
