9bffebcc5d58613740693e0befffb13606f91eea
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-05 17:32:41 +0900

    Adding a Newsletter Route

---
+++ pages/api/newsletter.js
@@ -0,0 +1,17 @@
+import { resolveSoa } from "dns";
+
+function handler(req, res) {
+	if (req.method === "POST") {
+		const userEmail = req.body.email;
+
+		if (!userEmail || !userEmail.includes("@")) {
+			res.status(422).json({ message: "Invalid email address." });
+			return;
+		}
+
+		console.log(userEmail);
+		res.status(200).json({ message: "Signed up!" });
+	}
+}
+
+export default handler;
