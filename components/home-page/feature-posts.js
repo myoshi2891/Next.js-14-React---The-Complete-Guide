e959fd6a9da6c9ff185c142fe0c0e226d701f889
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-08 16:24:33 +0900

    Starting Work On The "Featured Posts" Part

--- components/home-page/feature-posts.js
+++ components/home-page/feature-posts.js
@@ -1,5 +1,11 @@
+import classes from "./feature-posts.module.css";
+
 function FeaturePosts() {
-	return <div></div>;
+	return (
+		<section className={classes.latest}>
+			<h2>Featured Posts</h2>
+		</section>
+	);
 }
 
 export default FeaturePosts;
