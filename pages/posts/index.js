b81ef1602bf1ae69226361c8ca9f0865a3ab2258
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-08 21:28:48 +0900

    Adding the "All Posts" Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,41 @@
+import AllPosts from "../../components/posts/all-posts";
+const DUMMY_POSTS = [
+	{
+		slug: "getting-started-with-nextjs1",
+		title: "Getting Started with Nextjs",
+		image: "getting-started-nextjs.png",
+		excerpt: "NextJS is a the React framework for production",
+		date: "2024-09-08",
+	},
+	{
+		slug: "getting-started-with-nextjs2",
+		title: "Getting Started with Nextjs",
+		image: "getting-started-nextjs.png",
+		excerpt: "NextJS is a the React framework for production",
+		date: "2024-09-08",
+	},
+	{
+		slug: "getting-started-with-nextjs3",
+		title: "Getting Started with Nextjs",
+		image: "getting-started-nextjs.png",
+		excerpt: "NextJS is a the React framework for production",
+		date: "2024-09-08",
+	},
+	{
+		slug: "getting-started-with-nextjs4",
+		title: "Getting Started with Nextjs",
+		image: "getting-started-nextjs.png",
+		excerpt: "NextJS is a the React framework for production",
+		date: "2024-09-08",
+	},
+];
+
 function AllPostsPage() {
-	return <div></div>;
+	return (
+		<>
+			<AllPosts posts={DUMMY_POSTS} />
+		</>
+	);
 }
 
 export default AllPostsPage;
