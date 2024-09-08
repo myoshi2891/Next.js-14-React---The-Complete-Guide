5e21b760e330129face5be89aa145d9726366785
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-08 13:31:49 +0900

    Our Target State & Starting Project

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,27 @@
-import Head from 'next/head';
-
-import Layout from '../components/layout/layout';
-import '../styles/globals.css';
+import Head from "next/head";
+import Layout from "../components/layout/layout";
+import Notification from "../components/ui/notification";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name='description' content='NextJS Events' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  );
+	return (
+		<Layout>
+			<Head>
+				<title>Next Events</title>
+				<meta name="description" content="NextJS Events" />
+				<meta
+					name="viewport"
+					content="initial-scale=1.0, width=device-width"
+				/>
+			</Head>
+			<Component {...pageProps} />
+			<Notification
+				title="Test"
+				message="This is test"
+				status="pending"
+			/>
+		</Layout>
+	);
 }
 
 export default MyApp;
