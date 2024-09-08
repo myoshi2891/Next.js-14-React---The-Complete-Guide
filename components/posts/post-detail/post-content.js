d4c172900a210378f109855b35daba1050de73e3
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-08 21:45:21 +0900

    Working On The "Post Detail" Page

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,22 @@
+import PostHeader from "./post-header";
+import classes from "./post-content.module.css";
+
+const DUMMY_POST = {
+	slug: "getting-started-with-nextjs1",
+	title: "Getting Started with Nextjs",
+	image: "getting-started-nextjs.png",
+	date: "2024-09-08",
+	content: "# NextJS is a the React framework for production",
+};
+
+function PostContent() {
+	const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+	return (
+		<article className={classes.content}>
+			<PostHeader title={DUMMY_POST.title} image={imagePath} />
+			{DUMMY_POST.content}
+		</article>
+	);
+}
+
+export default PostContent;
