22da03a34aa24eddd4d3d58e88f79ed248a81178
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-11 23:24:26 +0900

    Rendering Code Snippets From Markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,6 +2,8 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 function PostContent(props) {
 	const { post } = props;
@@ -35,6 +37,18 @@ function PostContent(props) {
 			}
 
 			return <p>{paragraph.children}</p>;
+		},
+
+		code(code) {
+			const { className, children } = code;
+			const language = className.split("-")[1];
+			return (
+				<SyntaxHighlighter
+					style={atomDark}
+					language={language}
+					children={children}
+				/>
+			);
 		},
 	};
 	return (
