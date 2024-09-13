49d7434113558e66cc45ee1bc3a3171c8c917684
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-13 22:19:32 +0900

    Adding "head" Data

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import FeaturePosts from "../components/home-page/feature-posts";
 import Hero from "../components/home-page/hero";
 import { getFeaturedPosts } from "../lib/posts-util";
@@ -5,6 +6,13 @@ import { getFeaturedPosts } from "../lib/posts-util";
 function HomePage(props) {
 	return (
 		<>
+			<Head>
+				<title>My Blog</title>
+				<meta
+					name="description"
+					content="I post about programming and web development."
+				/>
+			</Head>
 			<Hero />
 			<FeaturePosts posts={props.posts} />
 		</>
--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,17 @@
 import AllPosts from "../../components/posts/all-posts";
 import { getAllPosts } from "../../lib/posts-util";
+import Head from "next/head";
 
 function AllPostsPage(props) {
 	return (
 		<>
+			<Head>
+				<title>All Posts</title>
+				<meta
+					name="description"
+					content="A list of all programming-related tutorials and posts!"
+				/>
+			</Head>
 			<AllPosts posts={props.posts} />
 		</>
 	);
