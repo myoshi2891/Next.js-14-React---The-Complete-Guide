f56a61a9593d9f0079aa4cb8ebeaa71461da1624
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-14 20:30:26 +0900

    Adding User Logout

--- components/layout/main-navigation.js
+++ components/layout/main-navigation.js
@@ -1,13 +1,15 @@
 import Link from 'next/link';
-import { useSession } from "next-auth/client";
+import { useSession, signOut } from "next-auth/client";
 
 import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
 	const [session, loading] = useSession();
 
-	console.log(session);
-	console.log(loading);
+	function logoutHandler() {
+		signOut();
+	}
+
 	return (
 		<header className={classes.header}>
 			<Link href="/">
@@ -27,8 +29,8 @@ function MainNavigation() {
 								<Link href="/profile">Profile</Link>
 							</li>
 							<li>
-								<button>Logout</button>
-							</li>{" "}
+								<button onClick={logoutHandler}>Logout</button>
+							</li>
 						</>
 					)}
 				</ul>
