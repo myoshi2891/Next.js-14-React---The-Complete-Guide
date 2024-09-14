ff797d604e7c9c18761935efbb964ca059757948
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-14 22:02:13 +0900

    Adding the "Change Password" Logic

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from "next-auth/client";
+import { hashPassword, verifyPassworrd } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
 
 async function handler(req, res) {
 	if (req.method !== "PATCH") {
@@ -11,6 +13,46 @@ async function handler(req, res) {
 		res.status(401).json({ message: "Not authenticated!" });
 		return;
 	}
+    
+    const userEmail = session.user.email;
+	const oldPassword = req.body.oldPassword;
+	const newPassword = req.body.newPassword;
+
+	const client = await connectToDatabase();
+	const usersCollection = client.db().collection("users");
+
+	const user = await usersCollection.findOne({
+		email: userEmail,
+	});
+
+	if (!user) {
+		res.status(401).json({ message: "User not found" });
+		client.close();
+		return;
+	}
+
+	const currentPassword = user.password;
+
+	const passwordsAreEqual = await verifyPassworrd(
+		oldPassword,
+		currentPassword
+	);
+
+	if (!passwordsAreEqual) {
+		res.status(403).json({ message: "Passwords do not match." });
+		client.close();
+		return;
+	}
+
+	const hashedPassword = await hashPassword(newPassword);
+
+	const result = await usersCollection.updateOne(
+		{ email: user.email },
+		{ $set: { password: hashedPassword } }
+	);
+
+	client.close();
+	result.status(200).json({ message: "Passwords are updated!" });
 }
 
 export default handler;
