fae9727bf05b98bde6b6742d759557171876e61c
myoshizumi <myoshizumi@alumni.berklee.edu>
2024-09-14 21:26:44 +0900

    Protecting the "Auth" Page

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useRef, useState } from "react";
 import { signIn } from "next-auth/client";
+import { useRouter } from "next/router"; 
 import classes from "./auth-form.module.css";
 
 async function createUser(email, password) {
@@ -24,6 +25,7 @@ function AuthForm() {
 	const emailInputRef = useRef();
 	const passwordInputRef = useRef();
 	const [isLogin, setIsLogin] = useState(true);
+	const router = useRouter();
 
 	function switchAuthModeHandler() {
 		setIsLogin((prevState) => !prevState);
@@ -43,6 +45,7 @@ function AuthForm() {
 			});
 
 			if (!result.error) {
+				router.replace("/profile");
 			}
 		} else {
 			try {
